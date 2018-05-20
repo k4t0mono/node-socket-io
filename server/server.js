5f30aed97156fa0e108324a2b5c1bdb9fa965cd8
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-20 12:45:47 -0300

    Lecture 123: Socket.io Rooms

--- server/server.js
+++ server/server.js
@@ -21,20 +21,22 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
-	
-	socket.emit('newMessage', generateMessage(
-		'Admin', 'Welcome to the chat app'
-	));
-
-	socket.broadcast.emit('newMessage', generateMessage(
-		'Admin', 'A new user joined',
-	));
 
 	socket.on('join', (params, callback) => {
 		if(!isRealString(params.name) || !isRealString(params.room)) {
 			callback('Name and room name are required.');
 
 		} else {
+			socket.join(params.room);
+	
+			socket.emit('newMessage', generateMessage(
+				'Admin', 'Welcome to the chat app'
+			));
+
+			socket.broadcast.to(params.room).emit('newMessage', generateMessage(
+				'Admin', `${params.name} has joined`,
+			));
+
 			callback();
 		}
 	});
